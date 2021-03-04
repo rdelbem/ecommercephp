57d019715d14dc1b60a2986097559d05eaed24b2
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-03-04 00:56:14 -0300

    page class created

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 
 require_once 'vendor/autoload.php';
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config( 'debug', true );
 
@@ -10,10 +13,9 @@ $app->get(
 	'/',
 	function() {
 
-		$sql     = new Hcode\DB\Sql();
-		$results = $sql->select( 'SELECT * FROM tb_users' );
+		$page = new Page();
 
-		echo json_encode( $results );
+		$page->setTpl("index");
 
 	}
 );
